edc5c8529404d0bad3ac97b775e54bc1d2ee7ac9
Linus Unnebäck <linus@folkdatorn.se>
2021-06-22 19:10:22 +0200

    Add documentation text to TypeScript properties

--- types.d.ts
+++ types.d.ts
@@ -1,19 +1,33 @@
 import * as React from 'react';
 
 export interface ColorPickerProps extends React.Props<ColorPicker> {
+  /** Use row or vertical layout */
   row?: boolean,
+  /** Enables snapping on the center of wheel and edges of wheel and slider */
   noSnap?: boolean,
+  /** Wheel color thumb size */
   thumbSize?: number,
+  /** Slider and slider color thumb size */
   sliderSize?: number,
+  /** Use swatchs of shades instead of slider */
   discrete?: boolean,
+  /** Show color swatches */
   swatches?: boolean,
+  /** If false swatches are shown before wheel */
   swatchesLast?: boolean,
+  /** Show swatch only and hide wheel and slider */
   swatchesOnly?: boolean,
+  /** Color of the color picker */
   color?: string,
+  /** If true the wheel thumb color is shaded */
   shadeWheelThumb?: boolean,
+  /** If true the slider thumb color is shaded */
   shadeSliderThumb?: boolean,
+  /** If true the slider thumb is reset to 0 value when wheel thumb is moved */
   autoResetSlider?: boolean,
+  /** Callback function for slider and wheel thumb movement */
   onColorChange?: (color: string) => void,
+  /** Callback function for when the slider and wheel thumb stops moving */
   onColorChangeComplete?: (color: string) => void,
 }
 
